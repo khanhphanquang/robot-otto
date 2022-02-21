a9fe1905fce2be5adfc0dabdf18845607920ecf7
khanhphanquang <85332137+khanhphanquang@users.noreply.github.com>
2022-02-21 13:08:06 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+basic.showIcon(IconNames.Heart)
+robotbit.Servo(robotbit.Servos.S1, 56)
+robotbit.Servo(robotbit.Servos.S2, 111)
+robotbit.Servo(robotbit.Servos.S3, 136)
+robotbit.Servo(robotbit.Servos.S4, 72)
+robotbit.Servo(robotbit.Servos.S5, 90)
+robotbit.Servo(robotbit.Servos.S6, 91)
+robotbit.Servo(robotbit.Servos.S7, 90)
+robotbit.Servo(robotbit.Servos.S8, 90)
+basic.forever(function () {
+	
+})
